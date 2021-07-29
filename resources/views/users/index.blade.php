f3a7f3fd4d584218700e0d369f38949ceee3b578
Jesús Emmanuel López Guerrero <jesusemm010@hotmail.com>
2021-07-28 22:59:38 -0500

    Quedo lista la parte de la creacion de usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,24 +14,36 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
-                    <div class="card">
-                        <form action="{{ route("users.store")}}" method="POST">
-                            <div class="form-row">
-                                <div class="col-sm-3">
-                                    <input type="text" name="name" class="form-control" placeholder="Nombre">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            @if ($errors->any())
+                                <div class="alerta alerta-danger">
+                                    @foreach ($errors->all() as $error)
+                                        -{{$error}} <br>
+                                    @endforeach
                                 </div>
-                                <div class="col-sm-4">
-                                    <input type="text" name="email" class="form-control" placeholder="Email">
+                            @endif
+                            <form action="{{ route("users.store")}}" method="POST">
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                        <input type="text" name="name" class="form-control" placeholder="Nombre"
+                                        value="{{ old("name") }}">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="email" class="form-control" placeholder="Email"
+                                        value="{{ old("email") }}">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </div>
                                 </div>
-                                <div class="col-sm-3">
-                                    <input type="password" name="password" class="form-control" placeholder="Contraseña">
-                                </div>
-                                <div class="col-auto">
-                                    @csrf
-                                    <button type="submit" class="btn btn-primary">Enviar</button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
+                        
                     </div>
                     <table class="table">
                         <thead>
